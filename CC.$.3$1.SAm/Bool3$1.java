7eafe9bde7b591744ada0bff6018687f4ca802c5
Conscience-Constellator <66545800+Conscience-Constellator@users.noreply.github.com>
2025-10-29 18:50:03 -0400

    Add files via upload

---
+++ CC.$.3$1.SAm/Bool3$1.java
@@ -0,0 +1,13 @@
+package CC.$.Q3$1;
+
+import CC.COd.Finishd;
+import CC.COd.Lin_DclAr;
+import CC.COd.Neds_Ovrid;
+import static CC.COd.Neds_Ovrid.*;
+
+@Finishd(Is_Finishd=true)
+public interface Bool3$1
+{
+	@Lin_DclAr @Neds_Ovrid(NEds=Yes) @Finishd(Is_Finishd=true)
+	boolean Bool3$1(boolean A,boolean B,boolean C);
+}
---
+++ CC.$.3$1.SAm/Int3$1.java
@@ -0,0 +1,13 @@
+package CC.$.Q3$1;
+
+import CC.COd.Finishd;
+import CC.COd.Lin_DclAr;
+import CC.COd.Neds_Ovrid;
+import static CC.COd.Neds_Ovrid.*;
+
+@Finishd(Is_Finishd=true)
+public interface Int3$1
+{
+	@Lin_DclAr @Neds_Ovrid(NEds=Yes) @Finishd(Is_Finishd=true)
+	int Int3$1(int A,int B,int C);
+}
